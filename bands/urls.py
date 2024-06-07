07dd6df5af263ef50f94be6fd6c47b538beaf51d
Marc Altmann <marcaltmann@posteo.de>
2024-06-07 14:27:47 +0200

    Finish chapter 4

--- bands/urls.py
+++ bands/urls.py
@@ -5,4 +5,7 @@ from bands import views
 urlpatterns = [
     path("musician/<int:musician_id>/", views.musician, name="musician"),
     path("musicians/", views.musicians, name="musicians"),
+    path("bands/", views.bands, name="bands"),
+    path("bands/<int:band_id>/", views.band, name="band"),
+    path("venues/", views.venues, name="venues"),
 ]
--- bands/views.py
+++ bands/views.py
@@ -1,7 +1,11 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
 
-from bands.models import Musician
+from bands.models import Musician, Band, Venue, Room
+
+DEFAULT_PAGE_NUMBER = 1
+DEFAULT_PAGE_SIZE = 2
+MAX_PAGE_SIZE = 100
 
 
 def musician(request, musician_id):
@@ -12,21 +16,47 @@ def musician(request, musician_id):
 
 def musicians(request):
     all_musicians = Musician.objects.all().order_by("last_name", "first_name")
-    paginator = Paginator(all_musicians, 2)
 
-    page_num = request.GET.get("page", 1)
+    page_num = request.GET.get("page", DEFAULT_PAGE_NUMBER)
     page_num = int(page_num)
+    page_size = request.GET.get("size", DEFAULT_PAGE_SIZE)
+    page_size = int(page_size)
+
+    paginator = Paginator(all_musicians, page_size)
 
     if page_num < 1:
         page_num = 1
     elif page_num > paginator.num_pages:
         page_num = paginator.num_pages
 
-    page = paginator.page(page_num)
+    if page_size < 2:
+        page_size = 1
+    elif page_size > MAX_PAGE_SIZE:
+        page_size = MAX_PAGE_SIZE
 
+    page = paginator.page(page_num)
     data = {
         "musicians": page.object_list,
         "page": page,
+        "page_size": page_size,
     }
 
     return render(request, "musicians.html", data)
+
+
+def bands(request):
+    all_bands = Band.objects.all().order_by("name")
+    data = {"bands": all_bands}
+    return render(request, "bands.html", data)
+
+
+def band(request, band_id):
+    band = get_object_or_404(Band, pk=band_id)
+    data = {"band": band}
+    return render(request, "band.html", data)
+
+
+def venues(request):
+    all_venues = Venue.objects.all().order_by("name")
+    data = {"venues": all_venues}
+    return render(request, "venues.html", data)
