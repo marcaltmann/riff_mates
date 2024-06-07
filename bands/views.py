6a1e7051250454c90a821a80a1585cf3b9bb948c
Marc Altmann <marcaltmann@posteo.de>
2024-06-07 09:31:28 +0200

    Try out pagination

--- bands/views.py
+++ bands/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
 
 from bands.models import Musician
@@ -10,6 +11,22 @@ def musician(request, musician_id):
 
 
 def musicians(request):
-    musicians = Musician.objects.all().order_by('last_name', 'first_name')
-    data = {'musicians': musicians}
+    all_musicians = Musician.objects.all().order_by('last_name', 'first_name')
+    paginator = Paginator(all_musicians, 2)
+
+    page_num = request.GET.get('page', 1)
+    page_num = int(page_num)
+
+    if page_num < 1:
+        page_num = 1
+    elif page_num > paginator.num_pages:
+        page_num = paginator.num_pages
+
+    page = paginator.page(page_num)
+
+    data = {
+        'musicians': page.object_list,
+        'page': page,
+    }
+
     return render(request, "musicians.html", data)
