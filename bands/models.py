e8d43e93c947e38b937f26809f2eab4135ea453c
Marc Altmann <marcaltmann@posteo.de>
2024-06-07 13:05:48 +0200

    Still chapter 4

--- bands/models.py
+++ bands/models.py
@@ -3,8 +3,31 @@ from django.db import models
 
 class Musician(models.Model):
     first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50, db_index=True)
     birth = models.DateField()
 
     def __str__(self):
-        return f'Musician(id={self.id}, last_name={self.last_name})'
+        return f"Musician(id={self.id}, last_name={self.last_name})"
+
+
+class Venue(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"Venue(id={self.id}, name={self.name})"
+
+
+class Room(models.Model):
+    name = models.CharField(max_length=50)
+    venue = models.ForeignKey(Venue, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Room(id={self.id}, name={self.name})"
+
+
+class Band(models.Model):
+    name = models.CharField(max_length=50)
+    musicians = models.ManyToManyField(Musician)
+
+    def __str__(self):
+        return f"Band(id={self.id}, name={self.name})"
